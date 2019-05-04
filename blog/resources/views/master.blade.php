ac70f1863e3fc1794039dd58268ed0001462c6f9
hungha1998 <16520470@gm.uit.edu.vn>
2019-05-04 22:25:18 +0700

    Update layout

--- blog/resources/views/master.blade.php
+++ blog/resources/views/master.blade.php
@@ -22,28 +22,90 @@
   @show
 </head>
 <body class="">
-<div>
-    <header style="text-align: center; background-color: #ddd; padding: 20px; position: relative">
-		<h2><a style="color: darkblue; text-transform: uppercase; font-weight: bold" href="{{url('/')}}">Hội Nghị Khoa Học</a><h2>
-    <a style="position: absolute; right: 10px; top: 18px"href="{{url('/login')}}"><small>Là người thuyết trình?</small></a>
-  </header>
+	<header style="text-align: center; background-color: #18478B; padding:20px; position: relative;">
+		<div class="row">
+			<div class="col-md-6">
+			<h2><img style= "margin-right:20px;"src="{{ asset('uploads/logo.png') }}" width="50"><a style="color: white; text-transform: uppercase; font-weight: bold" href="{{url('/')}}">Hội Nghị Khoa Học</a><h2>
+			</div>
+			<div class="col-md-6">
+				<h2 style="padding-top:10px;"><a style="color: white; text-transform: uppercase; font-weight: bold" href="{{url('/login')}}">Là người thuyết trình?</a><h2>
+			</div>
+		</div>
+	  </header>
+	<div class="hoinghi_img">
+	<div class="container">
+		<section class="content-header">
+		  <h3 class="list_hoinghi">
+			@section('title')
+			   Hội nghị tiêu biểu:
+			@show
+		  </h3>
+		</section>
+		<hr/>
+		<section class="content">
+			@yield('content')
+		</section>
+	</div>
+	</div>
+<div class="container body_hoinghi">
+	<div class="row">
+		<div class="col-md-7">
+			<section class="content_2">
+			@yield('content_2')
+			</section>
+		</div>
+		<div class="col-md-5">
+			<section class="content_1">
+			@yield('content_1')
+			</section>
+		</div>
+	</div>
 </div>
 
-<div class="container">
-    <section class="content-header">
-      <h3>
-        @section('title')
-           Danh Sách Hội Nghị
-        @show
-      </h3>
-    </section>
-    <hr/>
-    <section class="content">
-        @yield('content')
-    </section>
-  </div>
-  <footer class="main-footer">
-  </footer>
+<footer class="main-footer"style="text-align: center; background-color: #18478B; padding:20px; position: relative;">
+		<div class="cover">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-3">
+						<h3 style="color:white">Nav links</h3>
+						<a style="color:white" href="">Link 1</a><br>
+						<a style="color:white" href="">Link 2</a><br>
+						<a style="color:white" href="">Link 3</a><br>
+						<a style="color:white" href="">Link 4</a><br>
+					</div>
+					
+					<div class="col-sm-3">
+						<h3 style="color:white">Liên hệ</h3>
+						<address>
+						<p style="color:white" ><b>Địa chỉ: </b><i>Trường CNTT<br>
+							Linh Trung, Thủ Đức, HCM</i>
+						</p>
+						<p style="color:white"><b>Điện thoại: </b><i>0964030602</i>
+						</p>
+						<p style="color:white"><b>Mail: </b><a style="color:white" href="mailto:16520412@gm.uit.edu.vn" target="_blank"><i>16520412@gm.uit.edu.vn</i></a>
+						</p>
+						</address>
+					</div>
+
+					<div class="col-sm-3">
+						<h3 style="color:white">Dev Team</h3>
+						<p style="color:white">
+							<a style="color:white" href="https://www.facebook.com/phandinhminh.hieu" target="_blank">Phan Đình Minh Hiếu</a><br>
+							<a style="color:white" href="https://www.facebook.com/lanphuong.nguyen.1309" target="_blank">Nguyễn Thị Lan Phương</a><br>
+							<a style="color:white" href="https://www.facebook.com/H15101998" target="_blank">Phạm Bảo Hân</a><br>
+							<a style="color:white" href="https://www.facebook.com/profile.php?id=100010713276063"  target="_blank">Hà Kiệt Hùng</a>
+						</p>
+					</div>
+					<div class="col-sm-3">
+						<h3 style="color:white" >Follow Us</h3>
+						</br>
+						<a style= "padding-right:10px" href="https://www.facebook.com/phandinhminh.hieu" target="_blank"><img style="width: 30px; height:30px;" src="/uploads/facebook.png"/></a>
+						<a href="https://plus.google.com/110251690083587542042" target="_blank"><img style="width: 40px; height:40px;" src="/uploads/google.png"/></a>
+					</div>
+				</div>
+			</div>
+		</div>
+</footer>
   
 </div>
 <!-- jQuery 3 -->
--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -51,7 +51,11 @@
 </style>
 @endsection
 @section('content')
+@php
+$i = 0
+@endphp
 		@foreach($HoiNghis as $hoiNghi)
+		@if($i==0)
 		<a href="{{url('hoi-nghi',$hoiNghi->id)}}">
 			<div class="baiThuyetTrinhImage hoi-nghi-block" style='background: #ffffff url("{{url('/images',$hoiNghi->hinh)}}") no-repeat center center; background-size: cover;'>
 				<div class="overlay">
@@ -68,6 +72,67 @@
 			</div>
 		</a>
 		<hr>
-		@endforeach
+		@php
+			$i = 1
+			@endphp
+		@endif
 
+		@endforeach
+@endsection
+@section('content_1')
+		<div style="background-color: #18478B; padding:100px; text-align:center; margin-bottom:20px;">
+				<h1 style="font-size:30px;">H2</h1>
+				<h1 style="font-size:30px; color:white;"> Các hội nghị năm 2019 </h1>
+			</div>
+		@foreach($HoiNghis as $hoiNghi)
+		<a href="{{url('hoi-nghi',$hoiNghi->id)}}">
+			<div class="baiThuyetTrinhImage hoi-nghi-block" style='background: #ffffff url("{{url('/images',$hoiNghi->hinh)}}") no-repeat center center; background-size: cover;'>
+				<div class="overlay">
+					<div>
+						<a class="name text-shadow" style="font-size:20px;">{{$hoiNghi->tenHoiNghi}}</a>
+						<p class="text text-shadow" style="font-size:15px;">
+						{{$hoiNghi->thoiGianBatDau}} -> {{$hoiNghi->thoiGianKetThuc}} <br>
+						Địa Điểm: {{$hoiNghi->diaDiem}}<br>
+						<br>
+						{{$hoiNghi->moTa}}
+						</p>
+					</div>
+				</div>
+			</div>
+		</a>
+		<hr>
+
+		@endforeach
+@endsection
+@section('content_2')
+@php
+$i = 0
+@endphp
+		@foreach($HoiNghis as $hoiNghi)
+		@if($i==0)
+		<a href="{{url('hoi-nghi',$hoiNghi->id)}}">
+			<div class="baiThuyetTrinhImage hoi-nghi-block" style='background: #ffffff url("{{url('/images',$hoiNghi->hinh)}}") no-repeat center center; background-size: cover;'>
+			</div>
+			<div class="overlay">
+					<div>
+						<h1 class="name" style="font-size:50px; color: black">{{$hoiNghi->tenHoiNghi}}</h1>
+						<p>
+						<a style="font-size:20px; color: black">Thời gian bắt đầu: {{$hoiNghi->thoiGianBatDau}} -> {{$hoiNghi->thoiGianKetThuc}} </a> <br>
+						<a style="font-size:20px; color: black"> Địa Điểm: {{$hoiNghi->diaDiem}} </a><br>
+						<br>
+						<a style="font-size:20px; color: black">Mô tả: {{$hoiNghi->moTa}}</a><br>
+						<a style="font-size:20px; color: black">Số lượng tối đa: {{$hoiNghi->soLuongToiDa}}</a><br>
+						<a style="font-size:20px; color: black">Trạng thái: {{$hoiNghi->trangThai}}</a><br>
+						<a style="font-size:20px; color: black">Nội dung: {{$hoiNghi->noiDung}}</a><br>
+						</p>
+					</div>
+				</div>
+		</a>
+		<hr>
+		@php
+			$i = 1
+			@endphp
+		@endif
+
+		@endforeach
 @endsection
